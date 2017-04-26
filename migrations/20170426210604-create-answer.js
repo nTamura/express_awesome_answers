29a209c5b2ecd294e1a102cb6bee71a19a55fb2a
Nic Tamura <nictamura@gmail.com>
2017-04-26 14:24:39 -0700

    set associations with questions and answers

--- migrations/20170426210604-create-answer.js
+++ migrations/20170426210604-create-answer.js
@@ -12,7 +12,14 @@ module.exports = {
         type: Sequelize.TEXT
       },
       QuestionId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        //references prop creates a fk straight to db. model prop takes a val that is the table name of that this questionID should refer to. key property refers to the column inside the questions table that questionId pounts to
+        references: {
+          model: 'Questions', //use the table name
+          key: 'id'
+        },
+        onDelete: 'cascade', //if deleted, all answers will delete as well 
+        onUpdate: 'cascade'
       },
       createdAt: {
         allowNull: false,
--- models/question.js
+++ models/question.js
@@ -6,8 +6,15 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
-      }
+        Question.hasMany(models.Answer);
+        // list of instance methods added to Q by .hasMany
+        // Question#getAnswers
+        // Question#setAnswers
+        // Question#addAnswer
+        // Question#addAnswers
+
+
+        }
     }
   });
   return Question;
